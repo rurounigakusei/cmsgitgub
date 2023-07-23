487813d7c8d744c924de273d50608b1349b5bd49
achmad muhyidin <achmad.muhyidin91@gmail.com>
2023-07-23 19:46:56 +0700

    test ignoring db.php

--- db.php
+++ db.php
@@ -9,22 +9,10 @@
 //     define(strtoupper($key), $value);
 // } not worked in php 7
 
-//localhost ignored
 $DB_HOST = "localhost";
 $DB_USER = "root";
 $DB_PASS = "";
 $DB_NAME = "cms";
-
-//niagahoster
-// $DB_HOST = "localhost";
-// $DB_USER = "u485871302_cms";
-// $DB_PASS = "Projectcms123";
-// $DB_NAME = "u485871302_cms";
-
-// $DB_HOST = "sql212.infinityfree.com";
-// $DB_USER = "if0_34643018";
-// $DB_PASS = "fyriCTe2XN9";
-// $DB_NAME = "if0_34643018_cms";
 
 $connection = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 // if ($connection) {
